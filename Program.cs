713523b646c34a362e4e7c470d6a3dd9a13ce4c7
adaotilia <159614421+adaotilia@users.noreply.github.com>
2025-03-21 15:23:04 +0100

    deploy

--- Program.cs
+++ Program.cs
@@ -111,7 +111,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()|| true)
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
